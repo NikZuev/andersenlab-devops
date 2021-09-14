6b4811368374699469927d922f270314956ae4fe
NikZuev <89830184+NikZuev@users.noreply.github.com>
2021-09-14 19:57:53 +0300

    add full command

--- homework2/script.sh
+++ homework2/script.sh
@@ -2,6 +2,5 @@
 
 # run netstat
 echo "run netstat:"
-netstat_output=$(pwd)
-echo $netstat
-netstat -tunapl 
+netstat_output=$(netstat -tunapl | awk '/firefox/ {print $5}' | cut -d: -f1 | sort | uniq -c | sort | tail -n5 | grep -oP '(\d+\.){3}\d+' | while read IP ; do whois $IP | grep 'Organization' ; done)
+echo $netstat_output
